3db0fbdd3a07296db47edee3f47dad8950d9875a
Sean Norwood <norwood.sean@gmail.com>
2023-07-09 09:50:40 -0500

    Add code to replace numbers that contain 1
    Signed-off-by: Sean Norwood <norwood.sean@gmail.com>

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ document.getElementById("number-form").addEventListener("submit", function (even
   const number = document.getElementById("number").value;
   const outputEl = document.getElementById("output");
   const output = numberSubstitution(number);
+  console.log(output);
   outputEl.innerHTML = output.join(", ");
   outputEl.classList.add("show");
 });
 
 function numberSubstitution(number) {
   if (number === 0) return [0];
+  let output = [];
+
+  for (let i = 0; i <= number; i++) {
+    if (i.toString().includes("1")) {
+      output.push("Beep!");
+    } else {
+      output.push(i);
+    }
+  }
+
+  return output;
 }
